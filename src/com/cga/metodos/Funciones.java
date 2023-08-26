64175c1ec2c9868fffe55245a9db383cc173521c
Dan <dancursojava2023@gmail.com>
2023-08-26 18:31:30 +0100

    Añadidos ejercicio 12 y 13

--- src/com/cga/metodos/Funciones.java
+++ src/com/cga/metodos/Funciones.java
@@ -5,88 +5,87 @@ import java.util.Scanner;
 
 public class Funciones {
 
-	Scanner teclado = new Scanner(System.in);
+    Scanner teclado = new Scanner(System.in);
 
-	// Ejercicio 1
-	public void eco(int num1) {
-		for (int i = 0; i < num1; i++) {
-			System.out.print("Eco... ");
-		}
-	}
+    // Ejercicio 1
+    public void eco(int n1) {
+        for (int i = 0; i < n1; i++) {
+            System.out.print("Eco... ");
+        }
+    }
 
-	// Ejercicio 2
-	public void between(int num1, int num2) {
-		for (int i = num1 + 1; i < num2; i++) {
-			System.out.println("Los números entre " + num1 + " y " + num2 + " son " + i);
-		}
-	}
+    // Ejercicio 2
+    public void between(int n1, int n2) {
+        for (int i = n1 + 1; i < n2; i++) {
+            System.out.println("Los números entre " + n1 + " y " + n2 + " son " + i);
+        }
+    }
 
-	// Ejercicio 3
-	public void areaVolumen(double altura, double radio) {
+    // Ejercicio 3
+    public void areaVolumen(double altura, double radio) {
 
-		double area = 2 * Math.PI * radio * (altura + radio);
-		double volumen = Math.PI * (radio * radio) * altura;
+        double area = 2 * Math.PI * radio * (altura + radio);
+        double volumen = Math.PI * (radio * radio) * altura;
 
-		System.out.print("Para calcular el area pulse 1. Para calcular el volumen pulse 2: ");
-		int uNum = teclado.nextInt();
+        System.out.print("Para calcular el area pulse 1. Para calcular el volumen pulse 2: ");
+        int uNum = teclado.nextInt();
 
-		if (uNum == 1) {
-			System.out.printf("El cálculo del area es: %.2f", area);
-		} else if (uNum == 2) {
-			System.out.printf("El cálculo del volumen es: %.2f", volumen);
-		}
-	}
+        if (uNum == 1) {
+            System.out.printf("El cálculo del area es: %.2f", area);
+        } else if (uNum == 2) {
+            System.out.printf("El cálculo del volumen es: %.2f", volumen);
+        }
+    }
 
-	// Ejercicio 4
-	public int maximo(int num1, int num2) {
-        return Math.max(num1, num2);
-	}
+    // Ejercicio 4
+    public int maximo(int n1, int n2) {
+        return Math.max(n1, n2);
+    }
 
-	// Ejercicio 5
-	public boolean vocal(char voc) {
+    // Ejercicio 5
+    public boolean vocal(char voc) {
         return voc == 'a' || voc == 'e' || voc == 'i' || voc == 'o' || voc == 'u';
-	}
+    }
 
-	// Ejercicio 6
-	public boolean userVocal() {
-		System.out.print("Introduzca un carácter: ");
-		String voc = teclado.next();
+    // Ejercicio 6
+    public boolean userVocal() {
+        System.out.print("Introduzca un carácter: ");
+        String voc = teclado.next();
 
         return voc.equals("a") || voc.equals("e") || voc.equals("i") || voc.equals("o") || voc.equals("u");
-	}
+    }
 
-	// Ejercicio 7
-	public boolean esPrimo(int n) {
-		if (n <= 1) {
-			return false;
-		}
-		for (int i = 2; i <= Math.sqrt(n); i++) {
-			if (n % i == 0)
-				return false;
-		}
-		return true;
-	}
+    // Ejercicio 7
+    public boolean esPrimo(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
 
-	// Ejercicio 8
-	public int divPrimos(int n) {
-		int count = 0;
+    // Ejercicio 8
+    public int divPrimos(int n) {
+        int count = 0;
 
-		for (int i = 2; i <= n; i++) {
-			if (n % i == 0 && esPrimo(i)) {
-				count++;
-			}
-		}
-		return count;
-	}
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0 && esPrimo(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
 
-	// Ejercicio 9
-	public double calculadora(double n1, double n2) {
+    // Ejercicio 9
+    public double calculadora(double n1, double n2) {
 
-		double operatoria = 0;
+        double operatoria = 0;
 
-		System.out.println("Pulse 1 para sumar. Pulse 2 para restar. Pulse 3 para multiplicar. Pulse 4 para dividir. ");
-		int uNum = teclado.nextInt();
-
+        System.out.println("Pulse 1 para sumar. Pulse 2 para restar. Pulse 3 para multiplicar. Pulse 4 para dividir. ");
+        int uNum = teclado.nextInt();
 
         switch (uNum) {
             case 1 -> operatoria = n1 + n2;
@@ -94,25 +93,48 @@ public class Funciones {
             case 3 -> operatoria = n1 * n2;
             case 4 -> operatoria = n1 / n2;
         }
-		return operatoria;
-	}
+        return operatoria;
+    }
 
-	// Ejercicio 10
-	public int maximo(int num1, int num2, int num3) {
-		int result = Math.max(num1, num2);
-        return Math.max(result, num3);
-	}
+    // Ejercicio 10
+    public int maximo(int n1, int n2, int n3) {
+        int result = Math.max(n1, n2);
+        return Math.max(result, n3);
+    }
 
-	// Ejercicio 11
-	public double potencia(double base, int exp) {
+    // Ejercicio 11
+    public double potencia(double base, int exp) {
 
-		double resp;
+        double sol;
 
-		if (exp == 0) {
-			resp = 1.0;
-		} else {
-			resp = base * potencia(base, exp - 1);
-		}
-		return resp;
-	}
+        if (exp == 0) {
+            sol = 1.0;
+        } else {
+            sol = base * potencia(base, exp - 1);
+        }
+        return sol;
+    }
+
+    // Ejercicio 12
+    public int mcd(int a, int b) {
+
+        if (a == 0) {
+            return b;
+        }
+        if (b == 0) {
+            return a;
+        }
+
+        return (a >= b) ? mcd(a - b, b) : mcd(a, b - a);
+    }
+
+    // Ejercicio 13
+    public int fibonacci(int n) {
+
+        if (n <= 1) {
+            return n;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
--- src/com/cga/metodos/FuncionesMain.java
+++ src/com/cga/metodos/FuncionesMain.java
@@ -2,53 +2,60 @@ package com.cga.metodos;
 
 public class FuncionesMain {
 
-	public static void main(String[] args) {
-		Funciones fun = new Funciones();
-		
-				
-		// Ejercicio 1		
-		fun.eco(10);
-		System.out.println("\n");
-		
-		// Ejercicio 2
-		fun.between(-5, 3);
-		System.out.print("\n");
-		
-		// Ejercicio 3
-		fun.areaVolumen(10.25, 10.25);
-		System.out.println("\n");
-		
-		// Ejercicio 4
-		System.out.println("El mayor número de los dos es: " + fun.maximo(10, 15));
-		System.out.print("\n");
-		
-		// Ejercicio 5
-		System.out.print("¿El carácter es una vocal? " + fun.vocal('b'));
-		System.out.println("\n");
-		
-		// Ejercicio 6
-		System.out.print("¿El carácter que has introducido es una vocal? " + fun.userVocal());
-		System.out.println("\n");
-		
-		// Ejercicio 7
-		System.out.print("¿El número introducido es primo? " + fun.esPrimo(101));
-		System.out.println("\n");
-		
-		// Ejercicio 8
-		System.out.print("El número introducido tiene " + fun.divPrimos(100) + " divisores primos.");
-		System.out.println("\n");
-		
-		// Ejercicio 9
-		System.out.print("La colución de la operación es: " + fun.calculadora(10, 20));
-		System.out.println("\n");
-		
-		//Ejercicio 10
-		System.out.print("El mayor número de los tres es: " + fun.maximo(30, 55, 225));
-		System.out.println("\n");
-		
-		// Ejercicio 11
-		System.out.print("La potencia es igual a: " + fun.potencia(2, 3));
-		System.out.print("\n");
-	}
+    public static void main(String[] args) {
+
+        Funciones fun = new Funciones();
+
+        // Ejercicio 1
+        fun.eco(10);
+        System.out.println("\n");
+
+        // Ejercicio 2
+        fun.between(-5, 3);
+        System.out.print("\n");
+
+        // Ejercicio 3
+        fun.areaVolumen(10.25, 10.25);
+        System.out.println("\n");
+
+        // Ejercicio 4
+        System.out.println("El mayor número de los dos es: " + fun.maximo(10, 15));
+        System.out.print("\n");
+
+        // Ejercicio 5
+        System.out.print("¿El carácter es una vocal? " + fun.vocal('b'));
+        System.out.println("\n");
+
+        // Ejercicio 6
+        System.out.print("¿El carácter que has introducido es una vocal? " + fun.userVocal());
+        System.out.println("\n");
+
+        // Ejercicio 7
+        System.out.print("¿El número introducido es primo? " + fun.esPrimo(101));
+        System.out.println("\n");
+
+        // Ejercicio 8
+        System.out.print("El número introducido tiene " + fun.divPrimos(100) + " divisores primos.");
+        System.out.println("\n");
+
+        // Ejercicio 9
+        System.out.print("La solución de la operación es: " + fun.calculadora(10, 20));
+        System.out.println("\n");
+
+        //Ejercicio 10
+        System.out.print("El mayor número de los tres es: " + fun.maximo(30, 55, 225));
+        System.out.println("\n");
+
+        // Ejercicio 11
+        System.out.print("La potencia es igual a: " + fun.potencia(2, 3));
+        System.out.println("\n");
+
+        // Ejercicio 12
+        System.out.print("El máximo común divisor es: " + fun.mcd(14, 8));
+        System.out.println("\n");
+
+        // Ejercicio 13
+        System.out.print("El valor de Fibonacci en la posición seleccionada es: " + fun.fibonacci(15));
+    }
 
 }
