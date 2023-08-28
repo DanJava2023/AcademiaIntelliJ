e0c4fcfd4fe9685a6c5429ce382346f4bb9e74c2
Dan <dancursojava2023@gmail.com>
2023-08-28 16:32:55 +0100

    ArrayList

---
+++ src/com/cga/ArrayList/Coche.java
@@ -0,0 +1,42 @@
+package com.cga.ArrayList;
+
+public class Coche {
+    private String Matricula;
+    private String Marca;
+    private String Modelo;
+    private int Km;
+
+    public String getMatricula() {
+        return Matricula;
+    }
+
+    public void setMatricula(String Matricula) {
+        this.Matricula = Matricula;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public void setModelo(String Modelo) {
+        this.Modelo = Modelo;
+    }
+
+    public int getKm() {
+        return Km;
+    }
+
+    public void setKm(int Km) {
+        this.Km = Km;
+    }
+
+
+}
